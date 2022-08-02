99dc2ece7da3f719ccc8caad98615384ab175df6
Anton Kurakin <anton.kurakin@gmail.com>
2022-08-02 10:19:32 +0300

    Ok

---
+++ _send_whatsapp.py
@@ -0,0 +1,29 @@
+import pywhatkit
+# import os
+
+
+def send_message_inst():
+    mobile = input('Введите номер получателя: ')
+    # mobile = os.getenv('MOBILE')
+    message = input('Введите текст сообщения: ')
+    pywhatkit.sendwhatmsg_instantly(phone_no=mobile, message=message)
+
+
+def send_message():
+    mobile = input('Введите номер получателя: ')
+    # mobile = os.getenv('MOBILE')
+    message = input('Введите текст сообщения: ')
+    hour = int(input('Введите часы: '))
+    minutes = int(input('Введите минуты: '))
+    pywhatkit.sendwhatmsg(phone_no=mobile, message=message, time_hour=hour, time_min=minutes)
+
+
+def main():
+    # send_message_inst()
+    # send_message()
+    # pywhatkit.image_to_ascii_art(img_path='hack_archive.png')
+    # pywhatkit.send_hmail("mt5.rport.sender@gmail.com", "zphgsbdgkuvwsrcd", "Привет!", "<h1>HELLO</h1>", "anton.kurakin@gmail.com")
+
+
+if __name__ == '__main__':
+    main()
