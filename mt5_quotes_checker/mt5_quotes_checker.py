e08e51bb5e57d7d54eb1fd42c982c9b59d04ac66
Anton Kurakin <anton.kurakin@gmail.com>
2022-08-05 08:59:35 +0300

    Ok

--- mt5_quotes_checker/mt5_quotes_checker.py
+++ mt5_quotes_checker/mt5_quotes_checker.py
@@ -9,17 +9,17 @@ MANGER_LOGIN = '1029'
 MANGER_PASSWORD = 'dj805dktj85'
 urllib3.disable_warnings()
 logging.basicConfig(filename='mt5_quotes_checker.log', encoding='utf-8', level=logging.INFO)
-logging.info(str(datetime.datetime.now()) + ' Checker started')
+logging.info(str(datetime.datetime.now()) + ' - Checker started')
 
 try:
     while True:
         if qch.get_gateway_status(MT5_SERVER, MANGER_LOGIN, MANGER_PASSWORD, 0) == 0:
             logging.error(str(datetime.datetime.now()) + ' - Gateway ERROR')
-        time.sleep(10)
+        time.sleep(60)
 except Exception as ex:
     logging.error(f"{ex}\n" + str(datetime.datetime.now()) + " - Script ERROR")
 finally:
-    logging.info(str(datetime.datetime.now()) + ' Checker stopped')
+    logging.info(str(datetime.datetime.now()) + ' - Checker stopped')
 
 
 
